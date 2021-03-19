ea84e4f9f3352e46c082aeda92465392e249bc32
ludwingra <ludwing.ra@gmail.com>
2021-03-19 08:29:56 -0500

    Rutas, controller y configuracion de rutas en server

--- models/server.ts
+++ models/server.ts
@@ -1,14 +1,26 @@
 import express, { Application } from "express";
+import userRoutes from "../routes/usuario";
 
 
 class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        // Definir mis rutas
+        this.routes();
+    }
+
+
+    routes() {
+        this.app.use(this.apiPaths.usuarios, userRoutes)
     }
 
     listen () {
